ab8d8deec3656481b6d5b39605d96564c81cefa8
Vladimir <vovagrigoryan2003@gmail.com>
2025-02-22 19:36:22 +0300

    soso

--- server/hashPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function hashPassword() {
-    const password = 'Vovan376';
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log('Hashed password:', hashedPassword);
-}
-
-hashPassword();
